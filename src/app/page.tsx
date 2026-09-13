b49126f4e9e2909c60dd3a214a579d185df2be03
acquirofun <acquriofun@gmail.com>
2026-09-13 13:39:34 +0600

    Manual

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div style={{ margin: '30% auto 0px' , textAlign: 'center', padding: 'var(--spacing-xl)', fontSize: '1.25rem', fontWeight: '500'}}>
       Want to start the journey? Click the button below to connect your wallet and begin your adventure!
       <br/>
-      <a href = '/home'><button style={{ marginTop: 'var(--spacing-lg)', height: '40px', width: '120px', fontWeight: '700' }}>Let's Go!</button></a>
+      <a href = '/home'><button style={{ marginTop: 'var(--spacing-lg)', height: '40px', width: '120px', fontWeight: '700' }}>Let&apos;s Go!</button></a>
     </div>
   );
 }
