b1b129c865e4517a47a114ed0df8ad797ffd4560
acquirofun <acquirofun@gmail.com>
2026-09-15 23:16:09 +0600

    with ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 // src/app/layout.tsx
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
@@ -96,7 +97,94 @@ export default async function RootLayout({
       <body>
         <ContextProvider>
           <WalletSync />
+
           {children}
+
+          {/* BEGIN AADS AD UNIT 2455447 */}
+
+          <div style={{ position: "absolute", zIndex: 99999 }}>
+            <input
+              autoComplete="off"
+              type="checkbox"
+              id="aadsstickymu2x7ize"
+              hidden
+            />
+
+            <div style={{ paddingTop: 0, paddingBottom: 0 }}>
+              <div
+                style={{
+                  width: "200px",
+                  height: "200px",
+                  position: "fixed",
+                  textAlign: "center",
+                  fontSize: 0,
+                  top: "50%",
+                  transform: "translateY(-50%)",
+                  right: 0,
+                }}
+              >
+                <label
+                  htmlFor="aadsstickymu2x7ize"
+                  style={{
+                    top: "-24px",
+                    left: 0,
+                    position: "absolute",
+                    borderRadius: "4px",
+                    background: "rgba(248, 248, 249, 0.70)",
+                    padding: "4px",
+                    zIndex: 99999,
+                    cursor: "pointer",
+                  }}
+                >
+                  <svg
+                    fill="#000000"
+                    height="16px"
+                    width="16px"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 490 490"
+                  >
+                    <polygon points="456.851,0 245,212.564 33.149,0 0.708,32.337 212.669,245.004 0.708,457.678 33.149,490 245,277.443 456.851,490 489.292,457.678 277.331,245.004 489.292,32.337 " />
+                  </svg>
+                </label>
+
+                <div
+                  id="frame"
+                  style={{
+                    width: "200px",
+                    margin: "auto",
+                    zIndex: 99998,
+                    height: "auto",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                  }}
+                >
+                  <iframe
+                    data-aa="2455447"
+                    src="//ad.a-ads.com/2455447/?size=200x200"
+                    style={{
+                      border: 0,
+                      padding: 0,
+                      width: "200px",
+                      height: "200px",
+                      overflow: "hidden",
+                      margin: "0 auto",
+                    }}
+                  />
+                </div>
+              </div>
+
+              <style>
+                {`
+                  #aadsstickymu2x7ize:checked + div {
+                    display: none;
+                  }
+                `}
+              </style>
+            </div>
+          </div>
+
+          {/* END AADS AD UNIT 2455447 */}
         </ContextProvider>
       </body>
     </html>
---
+++ src/components/AadsStickyAd.tsx
@@ -0,0 +1,31 @@
+export default function AadsStickyAd() {
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: `<!-- BEGIN AADS AD UNIT 2455447 -->
+
+<div style="position: absolute; z-index: 99999">
+      <input autocomplete="off" type="checkbox" id="aadsstickymu2x7ize" hidden />
+      <div style="padding-top: 0; padding-bottom: 0;">
+        <div style="width:200px;height:200px;position:fixed;text-align:center;font-size:0;top:50%;transform:translateY(-50%);right:0">
+          <label for="aadsstickymu2x7ize" style="top: -24px; left: 0;; position: absolute;border-radius: 4px; background: rgba(248, 248, 249, 0.70); padding: 4px;z-index: 99999;cursor:pointer">
+            <svg fill="#000000" height="16px" width="16px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 490 490">
+              <polygon points="456.851,0 245,212.564 33.149,0 0.708,32.337 212.669,245.004 0.708,457.678 33.149,490 245,277.443 456.851,490 489.292,457.678 277.331,245.004 "/>
+            </svg>
+          </label>
+          <div id="frame" style="width: 200px;margin: auto;z-index: 99998;height: auto display: flex;flex-direction: column; justify-content: center">
+                        <iframe data-aa=2455447 src=//ad.a-ads.com/2455447/?size=200x200 style='border:0; padding:0; width:200px; height:200px; overflow:hidden; margin: 0 auto'></iframe>
+                    </div>
+        </div>
+        <style>
+      #aadsstickymu2x7ize:checked + div {
+        display: none;
+      }
+    </style>
+    </div></div>
+
+<!-- END AADS AD UNIT 2455447 -->`,
+      }}
+    />
+  );
+}
